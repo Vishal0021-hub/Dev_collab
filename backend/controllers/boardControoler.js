cfb79412e8afd260b6e3a6e55015cf0c9f42473b
Vishal Mishra <mishravishal1921@gmail.com>
2026-03-18 23:43:25 +0530

    board controller

---
+++ backend/controllers/boardControoler.js
@@ -0,0 +1,42 @@
+const Board = require("../models/Board");
+const Project = require("../models/Project");
+
+// Create Board
+exports.createBoard = async (req, res) => {
+  try {
+
+    const { name, projectId } = req.body;
+
+    // check project exists
+    const project = await Project.findById(projectId);
+
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    const board = await Board.create({
+      name,
+      project: projectId
+    });
+
+    res.status(201).json(board);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get Boards of a Project
+exports.getBoards = async (req, res) => {
+  try {
+
+    const { projectId } = req.params;
+
+    const boards = await Board.find({ project: projectId }).sort("order");
+
+    res.json(boards);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
